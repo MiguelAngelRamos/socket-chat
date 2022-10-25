496db9b61827ad32a8cd910eaa5a293464a9800e
Miguel Ramos <return.infox@gmail.com>
2022-10-25 19:58:11 -0300

    server socket coneccion2

--- server/index.js
+++ server/index.js
@@ -9,6 +9,17 @@ const io = require('socket.io')(server);
 
 app.use(express.static('client'));
 
+//* el primer elemento es el "Mensaje de Bienvenida"
+const messages = [
+  { id: 1, text: 'Bienvenido(a) al Chat privado con Socket.io y NodeJs', nickname: 'BotSocket' }
+];
+
+//* Abrir de la conexión de los sockets
+io.on('connection', (socket) => {
+  //* Esto se va encargar de recibir la conexión de los clientes que ingresen al server
+  console.log('Cliente conectado');
+  socket.emit('messages', messages);
+});
 //* Levantamos el Servidor
 server.listen(4200, () => {
   console.log(`El Servidor corriendo en http://localhost:4200`);
