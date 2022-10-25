1af627efdc66ec63f622541651e28318711fc027
Miguel Ramos <return.infox@gmail.com>
2022-10-25 20:34:42 -0300

    recibiendo el mensaje del servidor

--- client/js/main.js
+++ client/js/main.js
@@ -8,4 +8,20 @@ socket.on('connect', () => {
 
 socket.on('messages', (data) => {
   console.log(data);
+  render(data);
 })
+
+//* Una función para renderizar el contenido que nos envia el servidor en el index.html
+//* data un array, podemos utilizar el método map
+const render = data => {
+  const html = data.map((message) => {
+    return (`
+      <div class="message">
+        <strong>${message.nickname}</strong> dice:
+        <p>${message.text} </p>
+      </div>   
+    `)
+  }).join(' '); //* para tener un espacio entre los elementos
+
+  document.querySelector('#messages').innerHTML = html;
+}
